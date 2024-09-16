0f926add06629d05df27d65f3bf265743509e79a
Basile Gandon <gandon6u@etu.univ-lorraine.fr>
2024-09-16 14:58:39 +0200

    Added print_version

--- hello.cpp
+++ hello.cpp
@@ -1,12 +1,20 @@
 #include <iostream>
+#include <HelloProjConfig.h>
 
 void hello_world()
 {
     std::cout << "Hello world!\n";
 }
 
+void print_version()
+{
+    std::cout << "HelloProj version: " << HelloProj_VERSION_MAJOR << "."
+              << HelloProj_VERSION_MINOR << "\n";
+}
+
 int main()
 {
     hello_world();
+    print_version();
     return 0;
 }
