e4d355e7c89f4b9adaa9e88b1a2781c8fb9bfd3a
Basile Gandon <gandon6u@etu.univ-lorraine.fr>
2024-09-19 09:45:22 +0200

    Added lib

---
+++ include/basic/message.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <iostream>
+
+void hello_world();
+void print_version();
+void print_hardware_info();
--- tests/test-hello.cpp
+++ tests/test-hello.cpp
@@ -1,8 +1,7 @@
 #include <basic/message.h>
+
 int main()
 {
     hello_world();
-    print_version();
-    print_hardware_info();
     return 0;
 }
