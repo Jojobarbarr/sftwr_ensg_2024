d0ae55c62fb37eafb37d8a4118068bd286fd8f9e
Jojobarbarr <Jojobarbarr@users.noreply.github.com>
2024-09-23 15:01:30 +0000

    Apply format changes

--- mains/hello.cpp
+++ mains/hello.cpp
@@ -1,15 +1,12 @@
 #include <basic/message.h>
 #include <maths/power.h>
 
-
-
-
 #include <iostream>
 int main()
 {
     hello_world();
     print_version();
 
-    std::cout<<squared(2 );
+    std::cout << squared( 2 );
     return 0;
 }
