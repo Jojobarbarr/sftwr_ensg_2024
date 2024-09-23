eb8192c9b6689cc4e9ac50898695b3473db0327d
Basile Gandon <gandon6u@etu.univ-lorraine.fr>
2024-09-23 16:43:32 +0200

    BREAKING CHANGE: There is now a new library math.

--- mains/hello.cpp
+++ mains/hello.cpp
@@ -1,7 +1,9 @@
 #include <basic/message.h>
+#include <maths/power.h>
 int main()
 {
     hello_world();
     print_version();
+    squared(4);
     return 0;
 }
---
+++ src/maths/power.cpp
@@ -0,0 +1,6 @@
+#include <maths/power.h>
+
+#include <iostream>
+
+double squared(double num){double result(0)       ; result = num * num;
+return result;}
